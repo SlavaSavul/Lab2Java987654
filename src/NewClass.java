b135f497ffff42560610e80be8a0b6a9196aaab0
SlavaSavul <slavik_savul@mail.ru>
2018-02-20 10:23:43 +0300

    Initial commit

---
+++ src/NewClass.java
@@ -0,0 +1,11 @@
+
+import  newPackage.*;
+
+public class NewClass
+{
+    public static void main(String[] args)
+    {
+        ClassINewPackage d =new ClassINewPackage();
+        d.metod();
+    }
+}
---
+++ src/newPackage/ClassINewPackage.java
@@ -0,0 +1,16 @@
+package newPackage;
+
+public class ClassINewPackage
+{
+
+public void metod()
+{
+    newmetod();
+}
+//TODO:11
+    private void newmetod()
+    {
+        System.out.println("Hello world!");
+    }
+
+}
