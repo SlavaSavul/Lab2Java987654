7673e852af30de86cdb0415ec76268e26b825e46
SlavaSavul <slavik_savul@mail.ru>
2018-02-20 10:31:02 +0300

    sdfghn

--- src/newPackage/ClassINewPackage.java
+++ src/newPackage/ClassINewPackage.java
@@ -10,7 +10,9 @@ public void metod()
 //TODO:11
     private void newmetod()
     {
-        System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+
     }
 //asdfghjk
 }
